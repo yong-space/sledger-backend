d6e2d543dfbde19d9e714234bcfd5b2bedaf1819
Yong Sheng Tan <tanysh@pm.me>
2023-01-24 11:33:44 +0800

    Enable Async

--- src/main/java/tech/sledger/Sledger.java
+++ src/main/java/tech/sledger/Sledger.java
@@ -2,7 +2,9 @@ package tech.sledger;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
+@EnableAsync
 @SpringBootApplication
 public class Sledger {
     public static void main(String[] args) {
