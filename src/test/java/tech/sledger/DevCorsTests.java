61c92da4233b57fd69c0578f5310d0d585e79a76
Yong Sheng Tan <tanysh@pm.me>
2024-03-03 16:53:40 +0800

    Fix failing test

--- src/test/java/tech/sledger/DevCorsTests.java
+++ src/test/java/tech/sledger/DevCorsTests.java
@@ -1,18 +1,14 @@
 package tech.sledger;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.web.servlet.MockMvc;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.options;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
 
 @ActiveProfiles(profiles = "dev")
-@SpringBootTest
-@AutoConfigureMockMvc
-public class DevCorsTests {
+public class DevCorsTests extends BaseTest {
     @Autowired
     public MockMvc mvc;
 
