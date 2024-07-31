fc42685e8d043610bb9f06020f1c074e3d917fb8
Yong Sheng Tan <tanysh@pm.me>
2024-07-31 23:45:35 +0800

    Added native

--- src/main/java/tech/sledger/repo/TransactionRepo.java
+++ src/main/java/tech/sledger/repo/TransactionRepo.java
@@ -7,11 +7,11 @@ import java.time.Instant;
 import java.util.List;
 
 public interface TransactionRepo extends MongoRepository<Transaction, Long>, TransactionOpsRepo {
-    Transaction findFirstByOrderByIdDesc();
-    List<Transaction> findAllByAccountId(long accountId);
-    List<Transaction> findAllByAccountIdOrderByDate(long accountId);
-    List<Transaction> findAllByAccountIdInOrderByDate(List<Long> accountIds);
-    Transaction findFirstByAccountIdAndIdNotAndDateBeforeOrderByDateDesc(long accountId, long id, Instant date);
-    List<Transaction> findAllByAccountIdAndDateAfterOrderByDate(long accountId, Instant date);
-    List<Transaction> findAllByAccountIdAndDateBetween(long accountId, Instant after, Instant before);
+    <T extends Transaction> T findFirstByOrderByIdDesc();
+    <T extends Transaction> List<T> findAllByAccountId(long accountId);
+    <T extends Transaction> List<T> findAllByAccountIdOrderByDate(long accountId);
+    <T extends Transaction> List<T> findAllByAccountIdInOrderByDate(List<Long> accountIds);
+    <T extends Transaction> T findFirstByAccountIdAndIdNotAndDateBeforeOrderByDateDesc(long accountId, long id, Instant date);
+    <T extends Transaction> List<T> findAllByAccountIdAndDateAfterOrderByDate(long accountId, Instant date);
+    <T extends Transaction> List<T> findAllByAccountIdAndDateBetween(long accountId, Instant after, Instant before);
 }
