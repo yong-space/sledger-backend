33399f1be3e98c0949725757916a6ac1b7cffd12
Yong Sheng Tan <i@yong.space>
2025-04-19 18:27:37 +0800

    Added portfolio email endpoint

---
+++ src/main/java/tech/sledger/model/portfolio/EmailSnapshot.java
@@ -0,0 +1,17 @@
+package tech.sledger.model.portfolio;
+
+import lombok.Builder;
+import lombok.Data;
+import java.math.BigDecimal;
+import java.util.List;
+
+@Data
+@Builder
+public class EmailSnapshot {
+    private List<PortfolioPosition> positions;
+    private BigDecimal dayPnl;
+    private BigDecimal dayPnlPercent;
+    private BigDecimal netPnl;
+    private BigDecimal netPnlPercent;
+    private BigDecimal mktValue;
+}
